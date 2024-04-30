08a7a2ce1eae86f1fb11139fb74747359a354812
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-04-30 11:27:11 -0700

    upload lesson12.py

---
+++ lesson12.py
@@ -0,0 +1,26 @@
+"""
+إسماعيل 10
+سيدرا 4
+معن 11
+جابر 7
+مناف 6
+عبد الفتاح 9
+حسام 2
+أُبي 4
+غيث 3
+بهاء 9
+
+"""
+
+
+## upload into github
+"""
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Eng-Hasan-Hajjar/course-test.git
+git push -u origin main
+
+"""
+
