1f6f409a64c3e23b5160cc4168c40865762dc303
Eng-Hasan-Hajjar <eng.hasan.hajjar@gmail.com>
2024-05-03 13:13:02 -0700

    lesson 13

--- lesson13.py
+++ lesson13.py
@@ -1,13 +1,13 @@
 """
-ismail 14
-sedra 7
-maan 17
-jaber 11
-manaf 9
-Abd fattah  15
+ismail 15
+sedra 8
+maan 18
+jaber 12
+manaf 11
+Abd fattah  16
 hussam 2
-obia 6
-gith 4
+obia 7
+gith 5
 Bhaa 9
 lama 1
 
@@ -27,3 +27,44 @@ x=150
 y=250.2
 print("ex2 x", isinstance(x,float))
 print("ex2 y", isinstance(y,float))
+
+## المعاملات في البايثون
+## ex3
+print(2+9)
+
+
+## المعاملات
+## الحسابية
+## + - * / 
+## %
+## **
+## //
+
+##ex4
+
+x=10
+y=5
+z=3.5
+g=12
+h=4
+
+
+sume=x+y
+sub=x-h
+mul=x*g
+div=g/h
+modd=g%h
+expon=y**h
+dddfloor=g//y
+print("x=10 ,y=5, z=3.5 , g=12 ,h=4 ")
+print("ex4 :sume=x+y","sum =" ,sume)
+print("ex4:sub=x-h","sub =" ,sub)
+print("ex4 :mul=x*g","mul =" ,mul)
+print("ex4 :div=g/h","div =" ,div)
+print("ex4 :modd=g%h","modd =" ,modd)
+print("ex4: expon=y**h","expon =" ,expon)
+print("ex4: dddfloor=g//y","dddfloor =" ,dddfloor)
+
+## وظيفة
+## انشاء الة حاسبة بسيطة
+ 
